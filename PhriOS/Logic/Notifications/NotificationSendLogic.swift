59e5bc58031ebd7f0cb1e8b2f0cc7106bfa21642
basbuijsen <bbuijsen@gmail.com>
2022-04-15 13:17:48 +0200

    fixed notifications

--- PhriOS/Logic/Notifications/NotificationSendLogic.swift
+++ PhriOS/Logic/Notifications/NotificationSendLogic.swift
@@ -12,10 +12,7 @@ func setNotifications(times: [Date]){
     UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
     
     for date in times {
-        let calendar = Calendar.current
-        let hour = calendar.component(.hour, from: date)
-        let minutes = calendar.component(.minute, from: date)
-        scheduleNotification(hour: hour, minutes: minutes)
+        scheduleNotification(date: date)
     }
 }
 
@@ -23,59 +20,21 @@ func removeNotifications() {
     UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
 }
 
-func scheduleNotification(hour : Int, minutes: Int) {
-    //get enviroment
-    let defs = UserDefaults()
-    let adultMode = defs.bool(forKey: DEFS_ADULT_MODE)
-    
-    //create notification
+func scheduleNotification(date : Date) {
     let content = UNMutableNotificationContent()
     content.title = NOTIFICATION_TITLE
     content.body = NOTIFICATION_BODY
-    content.badge = 1
-    
-    //actions
-    let adultAction = UNNotificationAction(identifier:"adult",
-            title:"Direct invullen",options:[])
-    
-    let childAction = UNTextInputNotificationAction(identifier:
-        "child", title: "Billies invullen (kind)", options: [])
-    let parentAction = UNTextInputNotificationAction(identifier:
-        "parent", title: "Voor kind invullen (ouder)", options: [])
 
-    //create categories
-    let childCategory = UNNotificationCategory(identifier: "child",
-         actions: [childAction, parentAction],
-        intentIdentifiers: [], options: [])
-    
-    let adultCategory = UNNotificationCategory(identifier: "adult",
-         actions: [adultAction],
-        intentIdentifiers: [], options: [])
+    let triggerInputForHourlyRepeat = Calendar.current.dateComponents([.hour, .minute], from: date)
+    let trigger = UNCalendarNotificationTrigger.init(dateMatching: triggerInputForHourlyRepeat, repeats: true)
 
-    //select category for notification
-    content.categoryIdentifier = adultMode ? "adult" : "child"
-    
-    //id
-    let requestIdentifier = "Reminder"
-    
-    //time
-    var dateComponents = DateComponents()
-    dateComponents.hour = hour
-    dateComponents.minute = minutes
-    
-    
-    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-    
-    let request = UNNotificationRequest(identifier: requestIdentifier,
-            content: content, trigger: trigger)
-    
-    UNUserNotificationCenter.current().setNotificationCategories(
-                                [childCategory, adultCategory])
-    
-    UNUserNotificationCenter.current().add(request,
-            withCompletionHandler: { (error) in
-                // Handle error
-        })
+    let request = UNNotificationRequest(identifier: UUID().description, content: content, trigger: trigger)
+    let unc = UNUserNotificationCenter.current()
+    unc.add(request, withCompletionHandler: { (error) in
+        if let error = error {
+            print(error)
+        }
+    })
 }
 
 func requestPermission() {
