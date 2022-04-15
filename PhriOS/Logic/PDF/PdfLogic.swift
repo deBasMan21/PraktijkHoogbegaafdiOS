99bd3aaecc2604d23f4777ba918f1f559539def2
basbuijsen <bbuijsen@gmail.com>
2022-04-15 17:45:33 +0200

    added titles to pdf

--- PhriOS/Logic/PDF/PdfLogic.swift
+++ PhriOS/Logic/PDF/PdfLogic.swift
@@ -17,24 +17,23 @@ func createPDF(images : [UIImage], weekStatsParent : [Billie : Double]?, weekSta
     let adultMode = defs.bool(forKey: "adultMode")
     
     let pdfMetaData = [
-      kCGPDFContextCreator: "Praktijk Hoogbegaafd",
-      kCGPDFContextAuthor: "praktijkhoogbegaafd.nl"
+        kCGPDFContextCreator: "Praktijk Hoogbegaafd",
+        kCGPDFContextAuthor: "praktijkhoogbegaafd.nl"
     ]
     
     let format = UIGraphicsPDFRendererFormat()
     format.documentInfo = pdfMetaData as [String: Any]
-
+    
     let pageWidth = images[0].size.width * 3
-    let pageHeight = (images[0].size.height * (CGFloat(images.count) / 3)) + 600 + logo.size.height
+    let pageHeight = (images[0].size.height * (CGFloat(images.count) / 3)) + 800 + logo.size.height
     let pageRect = CGRect(x: 0, y: 0, width: pageWidth, height: pageHeight)
-
-
+    
     let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
-
+    
     let data = renderer.pdfData { (context) in
-
+        
         context.beginPage()
-
+        
         let attributes = [
             NSAttributedString.Key.font: UIFont.systemFont(ofSize: 72)
         ]
@@ -46,16 +45,26 @@ func createPDF(images : [UIImage], weekStatsParent : [Billie : Double]?, weekSta
         
         let attributesPurple = [
             NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 72),
-        NSAttributedString.Key.foregroundColor : UIColor(named: PHR_PURPLE)!
+            NSAttributedString.Key.foregroundColor : UIColor(named: PHR_PURPLE)!
         ]
         
         logo.draw(at: CGPoint(x: (pageWidth - logo.size.width) / 2, y: 0))
         
         var rowCount = 0
         var columnCount = 0
+        var titleCount = 1
+        
+        let titleOne = adultMode ? "Volwasenne" : (weekStatsParent != nil ? "Ouder" : "Kind")
+        let titleTwo = "Kind"
+        titleOne.draw(at: CGPoint(x: 50, y: logo.size.height), withAttributes: attributesPurple)
         
         for img in images {
-            img.draw(at: CGPoint(x: img.size.width * CGFloat(columnCount), y: img.size.height * CGFloat(rowCount) + logo.size.height))
+            if rowCount == 2 && columnCount == 0{
+                titleCount += 1
+                titleTwo.draw(at: CGPoint(x: 50, y: img.size.height * CGFloat(rowCount) + logo.size.height + 75), withAttributes: attributesPurple)
+            }
+            
+            img.draw(at: CGPoint(x: img.size.width * CGFloat(columnCount), y: img.size.height * CGFloat(rowCount) + logo.size.height + 75 * CGFloat(titleCount)))
             
             columnCount += 1
             if columnCount == 3 {
@@ -67,7 +76,7 @@ func createPDF(images : [UIImage], weekStatsParent : [Billie : Double]?, weekSta
         let weekParentTitle = adultMode ? "Weekstatistieken ouder:" : "Weekstatistieken:"
         let weekChildTitle = "Weekstatistieken kind:"
         
-        let statsY = (images[0].size.height * CGFloat(images.count / 3)) + logo.size.height + 50
+        let statsY = (images[0].size.height * CGFloat(images.count / 3)) + logo.size.height + 50 + (75 * CGFloat(titleCount))
         let statsChildX = weekStatsParent != nil ? (pageWidth - logo.size.width) / 2 : 50
         
         if weekStatsParent != nil {
